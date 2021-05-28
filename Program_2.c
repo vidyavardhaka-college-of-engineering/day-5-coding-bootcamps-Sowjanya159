ec69354ae0d7bfc1afaa23cc02c7eaa26176af7c
Sowjanya159 <84716946+Sowjanya159@users.noreply.github.com>
2021-05-29 00:47:13 +0530

    reverse str.c

--- Program_2.c
+++ Program_2.c
@@ -2,13 +2,28 @@
 //Input: vvce & Output: ecvv
 
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
-	char str[10];
+	char str[100],temp;
+	int i=0,j=0;
+	printf("Enter the string to be reversed ");
 	scanf("%s", str);
-
-	//Write your code here
+	
+	i=0;
+     j=strlen(str)-1;
+	
+     while(i<j)
+     {
+	  temp=str[i];
+	  str[i]=str[j];
+	  str[j]=temp;
+	  i++;
+	  j--;
+     }
+	
+     printf("The reversed string is %s",str);
 
 	return 0;
 }
