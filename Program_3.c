75dff12e0e10ebbbfabf2becdeac20b8beb7f0dd
Sowjanya159 <84716946+Sowjanya159@users.noreply.github.com>
2021-05-29 01:00:22 +0530

    Palindrome.c

--- Program_3.c
+++ Program_3.c
@@ -6,13 +6,32 @@
 //output:
 //Palindrome
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
-	char str[10];
+	char str[10],temp,r[10];
+	int i=0,n;
+	printf("enter the string");
 	scanf("%s", str);
+	strcpy(r,str);
+	n=strlen(str)-1;
 
 	//Write your code here
+	while(i<n)
+  {
+	temp=str[i];
+        str[i]=str[n];
+	str[n]=temp;
+	i++;
+	n--;
+  }
+printf("The reversed string is %s\n",str);
+	
+	if(strcmp(r,str)==0)
+	printf(%s is a palindrome\n",str);
+      else
+       printf("%s is not palindrome",str);
 	
 	return 0;
 }
